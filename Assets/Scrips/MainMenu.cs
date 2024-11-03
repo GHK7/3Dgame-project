cda6c457894281302e0594882106020906113e52
hongwei430 <rico669988@gmail.com>
2024-11-03 20:34:34 +0800

    選單

---
+++ Assets/Scrips/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("SampleScene");
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadSceneAsync("Credit");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackMenu()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+}
